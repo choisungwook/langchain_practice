fb14e39e4f6ed6e86a2884e40b4cd77516cbce8f
choisungwook <kgg1959@naver.com>
2023-12-17 01:54:53 +0900

    KAN-56 Add helloworld

--- main.py
+++ main.py
@@ -1,32 +1,36 @@
-from typing import List
-
 from langchain.llms import CTransformers
-from langchain.prompts.chat import ChatPromptTemplate
-from langchain.schema import BaseOutputParser
-
-
-class CommaSeparatedListOutputParser(BaseOutputParser[List[str]]):
-    """Parse the output of an LLM call to a comma-separated list."""
-
-    def parse(self, text: str) -> List[str]:
-        """Parse the output of an LLM call."""
-        return text.strip().split(", ")
-
-
-model = CTransformers(model="llama-2-7b-chat.Q5_K_M.gguf")
-
-template = "You are a helpful assistant that translates English to Korean."
-human_template = "{text}"
-
-chat_prompt = ChatPromptTemplate.from_messages(
-    [
-        ("system", template),
-        ("human", human_template),
-    ]
+from langchain.memory import ConversationBufferMemory
+from langchain.schema import SystemMessage
+from langchain.prompts import (
+    MessagesPlaceholder,
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
 )
+from langchain.chains import LLMChain
 
-chain = chat_prompt | model | CommaSeparatedListOutputParser()
-outputs = chain.invoke({"text": "Apple"})
 
-for output in outputs:
-    print(output)
+def getModel():
+    return CTransformers(model="llama-2-7b-chat.Q5_K_M.gguf")
+
+
+def generatePrompt():
+    template_messages = [
+        SystemMessage(content="You are a helpful assistant."),
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{text}"),
+    ]
+
+    return ChatPromptTemplate.from_messages(template_messages)
+
+
+model = getModel()
+prompt_template = generatePrompt()
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
+chain = LLMChain(llm=model, prompt=prompt_template, memory=memory)
+
+print(
+    chain.run(
+        text="What can I see in Vienna? Propose a few locations. Names only, no details."
+    )
+)
