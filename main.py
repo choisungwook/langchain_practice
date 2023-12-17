221eb4b3efc67dd861f9f2dd1de4bbb1cc5022e1
choisungwook <kgg1959@naver.com>
2023-12-17 22:25:35 +0900

    KAN-56 Remove unused package

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from typing import List
 
 from langchain.llms import CTransformers
+from langchain.prompts.chat import ChatPromptTemplate
+from langchain.schema import BaseOutputParser
 
 llm = CTransformers(model="llama-2-7b-chat.Q5_K_M.gguf")
 
